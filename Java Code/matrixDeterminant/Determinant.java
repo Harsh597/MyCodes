e67fa2a6add99c7e18ea64f490f917116e5d6c9b
HarshG <harshgupta597@gmail.com>
2021-07-14 10:18:43 +0530

    Improved implementation of the determinant by removing the unnecessary variable r

--- Java Code/matrixDeterminant/Determinant.java
+++ Java Code/matrixDeterminant/Determinant.java
@@ -2,7 +2,7 @@ package matrixDeterminant;
 
 public class Determinant {
 
-	static double det(double[][] mat, int r, int c, int rows, int cols) {
+	static double det(double[][] mat,int c, int rows, int cols) {
 		int ans = 0;
 		int sign = -1;
 		if (rows != cols) {
@@ -19,9 +19,9 @@ public class Determinant {
 				double[][] submat = new double[rows - 1][cols - 1];
 				int srow = 0;
 				int scol = 0;
-				for (int j = 0; j < rows; j++) {
+				for (int j = 1; j < rows; j++) {     // expanding determinant through first row i.e. skipping the first row
 					for (int k = 0; k < cols; k++) {
-						if (j == r || k == i)
+						if (k == i)
 							continue;
 						if (scol >= cols - 1) {
 							srow++;
@@ -34,8 +34,7 @@ public class Determinant {
 
 				}
 				// recursively solving the sub-determinant
-				ans += (sign *= -1) * mat[0][i] * det(submat, 0, i, rows - 1, cols - 1); // expanding determinant
-																							// through first row
+				ans += (sign *= -1) * mat[0][i] * det(submat,i, rows - 1, cols - 1); 
 			}
 			return ans;
 
@@ -46,18 +45,21 @@ public class Determinant {
 	public static void main(String[] args) {
 		Matrix mat1 = new Matrix();
 		Matrix mat2 = new Matrix();
-		mat1.set_Element(0, 0, 1);
-		mat1.set_Element(1, 2, 2);
-		mat1.set_Element(1, 2, 3);
-		mat1.set_Element(2, 1, 3);
-		mat1.set_Element(2, 2, 4);
+		mat1.set_Element(0, 0, 6);
+		mat1.set_Element(0, 1, 1);
+		mat1.set_Element(0, 2, 1);
+		mat1.set_Element(1, 0, 4);
+		mat1.set_Element(1, 1, -2);
+		mat1.set_Element(1, 2, 5);
+		mat1.set_Element(2, 0, 2);
+		mat1.set_Element(2, 1, 8);
+		mat1.set_Element(2, 2, 7);
 
 		mat1.get_Matrix();
 		mat2.get_Matrix();
 		System.out.println("After Multiplication:");
-		Matrix.mul(mat1, mat2);
-		//double [][]matrix=mat1.getMat();
-		System.out.println("Determinant : " + det(mat1.getMat(), 0, 0, mat1.getRows(), mat1.getCols()));
+		//Matrix.mul(mat1, mat2);
+		System.out.println("Determinant : " + det(mat1.getMat(), 0, mat1.getRows(), mat1.getCols()));
 
 	}
 
