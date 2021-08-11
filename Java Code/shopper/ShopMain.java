cb0f8d61ae774798f89a9ed16a4e7256147a67e5
HarshG <harshgupta597@gmail.com>
2021-08-11 10:34:02 +0530

    code for Bookshop object called in the shop main

--- Java Code/shopper/ShopMain.java
+++ Java Code/shopper/ShopMain.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class ShopMain {
 	public static void main(String[] args) {
 		Scanner inp = new Scanner(System.in);
-		String veg, frt;
+		String veg, frt,book;
 		int qt;
 		try {
 			String[] veggies = new String[] { "Arbi", "Aloo", "Bhendi", "Capsicum", "Dondakaya", "Turai", "Karela" };
@@ -13,17 +13,31 @@ public class ShopMain {
 			int[] quants = new int[] { 12, 30, 16, 15, 20, 12, 10 };
 			VegetableShop reddy = new VegetableShop(veggies, costs, quants);
 			FruitShop pradeep = new FruitShop();
-
+			BookShop mohit=new BookShop();
+			
+			System.out.println("Mohit's Book Shop");
+			mohit.listItem();
+			System.out.print("Enter Book Name and Qty: ");
+			book=inp.next();
+			qt=inp.nextInt();
+			double amount = mohit.buyItem(book,qt);
+			if(amount>0)
+				System.out.printf("Purchased %d Books of %s for Rs. %5.2f\n",qt,book,amount);
+			mohit.listItem();
+			
+			
 			System.out.println("Reddy's Vegetable Shop");
 			reddy.listItem();
 			System.out.print("Enter Vegetable Name and Qty: ");
 			veg = inp.next();
 			qt = inp.nextInt();
-			double amount = reddy.buyItem(veg, qt);
+			 amount = reddy.buyItem(veg, qt);
 			if (amount > 0)
 				System.out.printf("Purchased %d Kilos of %s for Rs. %5.2f\n", qt, veg, amount);
 			reddy.listItem();
 
+			
+			
 			System.out.println("Pradeep's Fruit Shop");
 			pradeep.listItem();
 			System.out.print("Enter Fruit Name and Qty: ");
